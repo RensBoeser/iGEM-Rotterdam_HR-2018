44e6298a7f83936a56a366f99240e704cbe0adc6
Rens Boeser <rbo@bridgevest.com>
2018-10-05 08:18:02 +0200

    turned on notebook generator

--- app/main.py
+++ app/main.py
@@ -24,18 +24,17 @@ def __main__():
 	_outputhtml = _dirpath + 'generated html/'
 	_notebookfiles = _libpath + 'notebook files/'
 	
-	# # Get entries
-	# entries = []
-	# entries.append(hardwareEntries(_libpath))
-	# entries.append(labEntries(_libpath))
+	# Get entries
+	entries = []
+	entries.append(hardwareEntries(_libpath))
+	entries.append(labEntries(_libpath))
 
-	# # Generate notebook
-	
-	# start = time.time()
-	# print('Generating notebook page...')
-	# NotebookGenerator(entries, _html, _notebookfiles).GeneratePage()
-	# end = time.time()
-	# print('Generated notebook page [{0}ms]'.format(int((end-start) * 1000)))
+	# Generate notebook
+	start = time.time()
+	print('Generating notebook page...')
+	NotebookGenerator(entries, _html, _notebookfiles).GeneratePage()
+	end = time.time()
+	print('Generated notebook page [{0}ms]'.format(int((end-start) * 1000)))
 	
 	# Generate pages
 	start = time.time()
