b9cf4e0de2516c716077b91c1d42618647f1f1de
Rens Boeser <rbo@bridgevest.com>
2018-10-16 15:37:20 +0200

    accordion

--- www/js/main.js
+++ www/js/main.js
@@ -1,2 +1,17 @@
 $('#navbar').load("http://2018.igem.org/Template:Rotterdam_HR/navbar?action=raw&ctype=text/html");
 $('#footer').load("http://2018.igem.org/Template:Rotterdam_HR/footer?action=raw&ctype=text/html");
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight){
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    } 
+  });
+}
